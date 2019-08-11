c000d594d0032334f55c0b10121067f847fa2e26
Omkar Kirpan <okirpan@gmail.com>
2019-08-11 21:38:45 +0530

    footer
    footer added

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function App() {
       <Contact />
       {/* <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2> */}
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
